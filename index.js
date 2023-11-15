045c5f878c28f62f26a49a4a8e3488bc9000e135
VERAPAO <pmnvera1984@gmail.com>
2023-11-15 20:47:01 -0300

    modificación en index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log("Hello world")
 
 console.log("Los límites que dividen la vida de la muerte son, en el mejor de los casos, sombríos y vagos. ¿Quién dirá dónde termina uno y dónde comienza el otro?")
+
+
+
+
+
+console.log("Hay acordes en los corazones de los más imprudentes que no pueden tocarse sin emoción.")
