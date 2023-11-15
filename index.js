2e686e09bafe047ee2957edebecaf513493f3b91
Rodrigo Toñanes <rodrigotonanes030@gmail.com>
2023-11-15 20:47:15 -0300

    agregando participantes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("Hello world")
 
 console.log("Los límites que dividen la vida de la muerte son, en el mejor de los casos, sombríos y vagos. ¿Quién dirá dónde termina uno y dónde comienza el otro?")
+
+console.log("participantes : paola vera , rodrigo toñanes");
