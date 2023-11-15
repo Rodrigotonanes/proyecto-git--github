055469818d6cd031acb7168d070247ce40aba51d
VERAPAO <pmnvera1984@gmail.com>
2023-11-15 20:40:09 -0300

    agregué frase en index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 console.log("Hello world")
+
+console.log("Los límites que dividen la vida de la muerte son, en el mejor de los casos, sombríos y vagos. ¿Quién dirá dónde termina uno y dónde comienza el otro?")
